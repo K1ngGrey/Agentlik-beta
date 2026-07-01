a435b0ce32c7f01b3d7fbb85b998a14f96e14a7a
king <asliddinabdullaev368@gmail.com>
2026-07-01 08:48:45 +0500

    feat: add ChatReadReceipts migration and update ChatMessages schema

---
+++ Back/src/DataAccess/Migrations/20260701034736_ChatReadReceipts.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChatReadReceipts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsPinned",
+                table: "ChatMessages",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsEdited",
+                table: "ChatMessages",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChatMessages_SenderId",
+                table: "ChatMessages",
+                column: "SenderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChatMessages_Users_SenderId",
+                table: "ChatMessages",
+                column: "SenderId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChatMessages_Users_SenderId",
+                table: "ChatMessages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ChatMessages_SenderId",
+                table: "ChatMessages");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsPinned",
+                table: "ChatMessages",
+                type: "boolean",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldDefaultValue: false);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsEdited",
+                table: "ChatMessages",
+                type: "boolean",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldDefaultValue: false);
+        }
+    }
+}
